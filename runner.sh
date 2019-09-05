a016554eaabcd9c66f5bc3fd32fdbb4a08148fe5
Sawyer Bergeron <sawyerbergeron@gmail.com>
2019-09-05 16:57:35 -0400

    upd

--- runner.sh
+++ runner.sh
@@ -12,4 +12,4 @@ git config --global user.name "Sawyer Bergeron"
 
 git add -A
 git commit -m "upd"
-ssh-agent bash -c 'ssh-add /home/sawyer/.ssh/id_rsa.pub; git push'
+ssh-agent bash -c 'ssh-add /home/sawyer/.ssh/id_rsa; git push'
