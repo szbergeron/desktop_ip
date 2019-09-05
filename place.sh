2985f0123f6abdb5465469e9b5937f75951c187d
Sawyer Bergeron <sawyerbergeron@gmail.com>
2019-09-05 19:37:19 -0400

    fix place.sh

--- place.sh
+++ place.sh
@@ -8,9 +8,9 @@ cd ~/dip/desktop_ip/
 
 git pull > /dev/null 2>&1
 
-git add -A > /dev/null 2>&1
-git commit -m "fix other ip" > /dev/null 2>&1
-git push > /dev/null 2>&1
+#git add -A > /dev/null 2>&1
+#git commit -m "fix other ip" > /dev/null 2>&1
+#git push > /dev/null 2>&1
 
 actual_ip=$(cat README.md | grep "inet 10" | grep -e "10\.[0-9]*\.[0-9]*\.[0-9]*" -o -m 1 | head -1)
 
