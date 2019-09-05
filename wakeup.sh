04b716d1ec13ddf43d9ba12d70abcdbf45c5c3eb
Sawyer Bergeron <sawyerbergeron@gmail.com>
2019-09-05 19:51:41 -0400

    going into sleep...

--- wakeup.sh
+++ wakeup.sh
@@ -8,9 +8,14 @@ cd ~/ip
 
 rm wakeup
 echo "sleeping" > wakeup
+
+git config --global user.email "sawyerbergeron@gmail.com"
+git config --global user.name "Sawyer Bergeron"
+git config --global url.ssh://git@github.com/.insteadOf https://github.com/
+
 git add -A
 git commit -m "going into sleep..."
-git push
+ssh-agent bash -c 'ssh-add /home/sawyer/.ssh/id_rsa; git push'
 
 while [true]
 do
